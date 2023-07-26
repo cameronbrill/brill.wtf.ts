fb1ee3db4858c302ece8c15e7600ee5244b76e37
Cameron Brill <c00brill@gmail.com>
2023-07-25 21:57:56 -0700

    chore: remove donate command

--- src/utils/bin/commands.ts
+++ src/utils/bin/commands.ts
@@ -44,15 +44,6 @@ export const resume = async (args: string[]): Promise<string> => {
   return 'Opening resume...';
 };
 
-// Donate
-export const donate = async (args: string[]): Promise<string> => {
-  return `thank you for your interest. 
-here are the ways you can support my work:
-- <u><a class="text-light-blue dark:text-dark-blue underline" href="${config.donate_urls.paypal}" target="_blank">paypal</a></u>
-- <u><a class="text-light-blue dark:text-dark-blue underline" href="${config.donate_urls.patreon}" target="_blank">patreon</a></u>
-`;
-};
-
 // Contact
 export const email = async (args: string[]): Promise<string> => {
   window.open(`mailto:${config.email}`);
