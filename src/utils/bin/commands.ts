aad60753323ed63791fd651cbf0fc82e30d736ba
Cameron Brill <c00brill@gmail.com>
2023-07-25 21:58:30 -0700

    feat: show sudo output before the rick roll

--- src/utils/bin/commands.ts
+++ src/utils/bin/commands.ts
@@ -126,7 +126,10 @@ export const emacs = async (args?: string[]): Promise<string> => {
 };
 
 export const sudo = async (args?: string[]): Promise<string> => {
-  window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ', '_blank'); // ...I'm sorry
+  setTimeout(
+    () => window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ', '_blank'),
+    350,
+  );
   return `Permission denied: with little power comes... no responsibility? `;
 };
 
